b3a4dc575a0628c2e568354d530c585bcaa16a71
Lymeng <lymengchun@gmail.com>
2021-03-23 12:02:25 +0700

    login activity

---
+++ app/src/main/java/com/example/chattingapp/activity_sign_up.java
@@ -0,0 +1,13 @@
+package com.example.chattingapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity_sign_up extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
